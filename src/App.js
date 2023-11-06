598be6904b231efc467e4730caa5aa9e51d63091
ElRharbiBadr <stardust12003@gmail.com>
2023-11-06 16:00:56 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+// import Bb from"./comp" //isem dial file comp 
+import './App.css';
+import Comp3 from "./comp3";
+import Comp2 from './comp2';
+import UseRef from './UseRef';
+import Calcul from './comp';
+import Compimg from './tpreact/slider/src/Compimg';
+import Todolist from './tpreact/todolist/src/Composants/todolist';
+function App() {
+  
+
+  return (
+    <div className="App">
+{/*   
+    <Comp2/> */}
+    <Todolist/>
+    </div>
+  );
+}
+
+export default App;
